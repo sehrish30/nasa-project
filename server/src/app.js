96568bceb5942b80dbee12c5612f305b3afb0378
sehrish30 <sehrishwaheed98@gmail.com>
2022-10-12 13:30:22 +0300

    Serving react frontend in production

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const planetsrouter = require("./routes/planets/planets.router");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 //middlewares
 app.use(
@@ -10,6 +11,11 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsrouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
